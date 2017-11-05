b9c49a6560b7a784b03d5f530182d4981fddf5de
caio <caiopcarvalho@outlook.com>
2017-11-05 19:14:01 -0200

    gerando as páginas radômica

--- crowlerUsers.py
+++ crowlerUsers.py
@@ -1,4 +1,6 @@
 import requests, csv
+from random import randint
+from bs4 import BeautifulSoup
 
 i=1
 
@@ -6,7 +8,7 @@ with open('userID.csv', 'w') as f:
     writeit = csv.writer(f, delimiter=',')
 
     for i in range(1,3): #[1,2[
-        url = "http://www.englishbaby.com/findfriends/gallery/detail/1"
+        url = "http://www.englishbaby.com/findfriends/gallery/detail/" + str(randint(1,2000))
         r = requests.get(url)
         print (r.url, r.status_code)
         plain_text = r.text 
