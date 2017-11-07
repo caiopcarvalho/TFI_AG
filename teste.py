69629929877ff561c255f59d46928c592d4e48d6
caio <caiopcarvalho@outlook.com>
2017-11-07 01:58:13 -0200

    .

--- teste.py
+++ teste.py
@@ -4,3 +4,13 @@ from random import randint
 print("Olá mundo")
 print("Git com vs")
 print(randint(1,1999))
+
+with open('userURL.csv','r') as filecsv:
+   kkk = csv.reader(filecsv, delimiter='/')
+         for row in reader:
+          writeFile(row[0])
+
+def writeFile(pk):
+   with open('userID.csv', 'w') as f:
+   data = csv.writer(f)
+    data.writerow(pk);
